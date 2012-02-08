ef49b351cf9787235c5daaeff2413491a306fbe1
Alexandru Guduleasa <gulyan89@yahoo.com>
2012-02-08 19:17:56 +0200

    Entry point
    Add main file to project.

---
+++ src/gulyan/project/Main.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package gulyan.project;
+
+/**
+ * Program entry point
+ * 
+ * @author gulyan
+ *
+ */
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
