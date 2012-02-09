29632a571e6cbd8d7578e4e903e8deacbbcbe38c
Alexandru Guduleasa <gulyan89@yahoo.com>
2012-02-09 14:15:43 +0200

    Stack stub

---
+++ src/gulyan/ds/Stack.java
@@ -0,0 +1,35 @@
+package gulyan.ds;
+
+public class Stack<T> implements OList<T> {
+
+	@Override
+	public void push(T e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public T pop() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public T top() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean isEmpty() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void clear() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
