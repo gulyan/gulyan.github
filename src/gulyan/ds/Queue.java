ab5950ee9c962f8ed4fd807ded90e6f13f612bbc
Alexandru Guduleasa <gulyan89@yahoo.com>
2012-02-09 16:37:46 +0200

    Queue implementation
    Implement class Queue using 2 stacks s1 and s2.
    The queue use the following rules:
    - all push operations are done on s1
    - all pop and top operations are done on s2
    - if s2 is empty pop all the elements from s1 and push them in s2
    - queue is empty if both stacks are empty

--- src/gulyan/ds/Queue.java
+++ src/gulyan/ds/Queue.java
@@ -1,35 +1,47 @@
 package gulyan.ds;
 
 public class Queue<T> implements OList<T> {
+	
+	Stack<T> s1, s2;
 
+	Queue() {
+		s1 = new Stack<T>();
+		s2 = new Stack<T>();
+	}
+	
+	private void dump() {
+		if(s2.isEmpty()) {
+			while(!s1.isEmpty())
+				s2.push(s1.pop());
+		}
+	}
+	
 	@Override
 	public void push(T e) {
-		// TODO Auto-generated method stub
-		
+		s1.push(e);
 	}
-
+	
 	@Override
 	public T pop() {
-		// TODO Auto-generated method stub
-		return null;
+		dump();
+		return s2.pop();
 	}
 
 	@Override
 	public T top() {
-		// TODO Auto-generated method stub
-		return null;
+		dump();
+		return s2.top();
 	}
 
 	@Override
 	public boolean isEmpty() {
-		// TODO Auto-generated method stub
-		return false;
+		return s1.isEmpty() && s2.isEmpty();
 	}
 
 	@Override
 	public void clear() {
-		// TODO Auto-generated method stub
-		
+		s1.clear();
+		s2.clear();
 	}
 
 }
