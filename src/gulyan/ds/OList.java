3f3993cc7fc468f86d803d3b17dcc8ced34186fb
Alexandru Guduleasa <gulyan89@yahoo.com>
2012-02-09 16:23:41 +0200

    OList specifications
    Comment the methods in interface OList

--- src/gulyan/ds/OList.java
+++ src/gulyan/ds/OList.java
@@ -9,14 +9,35 @@ package gulyan.ds;
  */
 public interface OList<T> {
 
+	/**
+	 * Add element to list
+	 * @param e element
+	 */
 	void push(T e);
 	
+	/**
+	 * Get next element from list and remove it from list
+	 * @see top
+	 * @return element
+	 */
 	T pop();
 	
+	/**
+	 * Get next element from list
+	 * @see pop
+	 * @return element
+	 */
 	T top();
 	
+	/**
+	 * Test if list is empty
+	 * @return true if list is empty, false otherwise
+	 */
 	boolean isEmpty();
 	
+	/**
+	 * Remove all elements from list
+	 */
 	void clear();
 	
 }
